fe1d4220f22dc777702ad24680123e2b8f9ba785
堀之内知行 <tomoyuki.horinouchi@gmail.com>
2025-11-13 14:40:36 +0900

    Update 検索前.js
    コミット

--- NSK/見積回答/検索前.js
+++ NSK/見積回答/検索前.js
@@ -1,4 +1,3 @@
-
 setInit();
 
 function setInit() {
@@ -9,13 +8,10 @@ function setInit() {
     var IDSIIRESAKI = condiMap['IDSIIRESAKI'];
     var conn = TALON.getDbConfig();
 
-    var cnt = getDtlCnt(conn, NRHATTYU, IDSIIRESAKI);
-
-    if (0 < cnt) return;
-
     var hinmokuMapList = getHinmokuMapList(conn, NRHATTYU);
+    var existingDtlMap = getDtlMap(conn, NRHATTYU, IDSIIRESAKI);
 
-    TalonDbUtil.begin(conn);
+    var insertList = [];
     for (var i = 0; i < hinmokuMapList.length; i++) {
 
         var hinmokuMap = hinmokuMapList[i];
@@ -24,7 +20,10 @@ function setInit() {
         var QTHATTYU = hinmokuMap['QTHATTYU'];
         var DTNOUKI = hinmokuMap['DTNOUKI'];
 
-        var insMap = {}
+        var existingDtl = existingDtlMap[IDHINMOKU];
+        if (!hasDifference(existingDtl, hinmokuMap)) continue;
+
+        var insMap = {};
         insMap['NRHATTYU'] = NRHATTYU;
         insMap['NRMITSUMORI'] = NRMITSUMORI;
         insMap['IDSIIRESAKI'] = IDSIIRESAKI;
@@ -33,29 +32,50 @@ function setInit() {
         insMap['QTHATTYU'] = QTHATTYU;
         insMap['DTNOUKI'] = DTNOUKI;
 
-        insertByMapEx(conn, "NP_MITSUMORI_DTL", insMap, false)
+        insertList.push(insMap);
+    }
+
+    if (insertList.length === 0) return;
+
+    TalonDbUtil.begin(conn);
+    for (var j = 0; j < insertList.length; j++) {
+        insertByMapEx(conn, "NP_MITSUMORI_DTL", insertList[j], false);
     }
     TalonDbUtil.commit(conn);
 }
 
-
-function getDtlCnt(conn, NRHATTYU, IDSIIRESAKI) {
+function getDtlMap(conn, NRHATTYU, IDSIIRESAKI) {
 
     var whereMap = {
-
         NRHATTYU: NRHATTYU,
         IDSIIRESAKI: IDSIIRESAKI
+    };
+
+    var dtlList = selectList(conn, "NP_MITSUMORI_DTL", null, whereMap, null);
+    var result = {};
+
+    for (var i = 0; i < dtlList.length; i++) {
+        var dtl = dtlList[i];
+        result[dtl['IDHINMOKU']] = dtl;
     }
 
-    return getCount(conn, "NP_MITSUMORI_DTL", whereMap);
+    return result;
+}
+
+function hasDifference(existingDtl, hinmokuMap) {
+
+    if (!existingDtl) return true;
+
+    return existingDtl['TXHINMOKU'] !== hinmokuMap['TXHINMOKU']
+        || existingDtl['QTHATTYU'] !== hinmokuMap['QTHATTYU']
+        || existingDtl['DTNOUKI'] !== hinmokuMap['DTNOUKI'];
 }
 
 function getHinmokuMapList(conn, NRHATTYU) {
 
     var whereMap2 = {
-
         NRHATTYU: NRHATTYU
-    }
+    };
 
     return selectList(conn, "NP_MITSUMORI_HINMOKU", null, whereMap2, null);
 }
